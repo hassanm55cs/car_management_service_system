0da3a151bd37b8b28f5b2852dc8078a5f7f16265
mostafa hassan <mostafa_hassan3@icloud.com>
2026-09-14 23:40:06 +0300

    test is added

---
+++ Model/test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car_services.Model
+{
+    public class test
+    {
+        public int suii{ get; set; }
+        public string name{ get; set; }
+    }
+}
